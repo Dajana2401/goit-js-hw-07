c58b0658de2c34afecaaa20b27cc97bbc3dd3cb0
Dajana <black.inez2401@gmail.com>
2023-12-26 15:15:18 +0100

    zadanie drugie

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsList=document.getElementById("ingredients");
+ingredients.forEach(ingredient=>{
+const ingList=document.createElement("li");
+const ingName=ingList.textContent=ingredient;
+console.log(ingName);
+ingList.classList.add('item');
+ingredientsList.append(ingList);
+console.log(ingList);
+});
